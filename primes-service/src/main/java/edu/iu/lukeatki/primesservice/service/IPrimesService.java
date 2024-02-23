77ca5d791f5f27b3fad027e7d188e6ffc657a86a
lukeatki <luke24aal@gmail.com>
2024-02-22 23:41:41 -0500

    initial commit

---
+++ primes-service/src/main/java/edu/iu/lukeatki/primesservice/service/IPrimesService.java
@@ -0,0 +1,5 @@
+package edu.iu.lukeatki.primesservice.service;
+
+public interface IPrimesService {
+    boolean isPrime(long n);
+}
---
+++ primes-service/src/test/java/edu/iu/lukeatki/primesservice/PrimesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.lukeatki.primesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
